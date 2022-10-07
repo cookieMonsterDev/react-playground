28c076b319c23dd2a7f4dd7813088eb9c774bf4d
It-is-a-piece-of-cake <mykhailo.toporkov@gmail.com>
2022-10-07 09:13:00 +0300

    feat: new Drop menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import Loader from './components/Loader/Loader';
-import DropDownMenu from './components/DropDownMenu/DropDownMenu1';
+// import DropDownMenu1 from './components/DropDownMenu/DropDownMenu1';
+import DropDownMenu2 from './components/DropDownMenu/DropDownMenu2';
 
 function App() {
   return (
     <div>
-      <DropDownMenu />
+      <DropDownMenu2 />
     </div>
   );
 }
--- src/components/DropDownMenu/DropDownMenu1.js
+++ src/components/DropDownMenu/DropDownMenu1.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 
-const DropDownMenu = () => {
+const DropDownMenu1 = () => {
   const [active, setActive] = useState(false);
 
   return (
@@ -33,7 +33,7 @@ const DropDownMenu = () => {
   );
 };
 
-export default DropDownMenu;
+export default DropDownMenu1;
 
 const Window = styled.div`
   width: 100vw;
